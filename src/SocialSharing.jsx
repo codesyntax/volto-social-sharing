e329e314df845bf7ccb3525df17aef06c11e4f5f
bipoza <bipoza@gmail.com>
2021-01-22 12:47:36 +0100

    minimal addon structure

---
+++ src/SocialSharing.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import './volto-cookie-banner.css';
+
+const SocialSharing = () => (
+    <div>
+    <h1>KKKKKKKKK</h1>
+    </div>
+//   <CookieConsent
+//     buttonText={buttonText ?? 'OK'}
+//     acceptOnScroll={acceptOnScroll}
+//   >
+//     <div dangerouslySetInnerHTML={{ __html: flattenHTMLToAppURL(text) }} />
+//   </CookieConsent>
+);
+
+// CookieBanner.propTypes = {
+//   text: PropTypes.string.isRequired,
+//   buttonText: PropTypes.string,
+//   acceptOnScroll: PropTypes.bool,
+// };
+
+export default SocialSharing;
