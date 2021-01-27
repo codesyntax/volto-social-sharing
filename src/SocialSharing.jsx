2e67a43553192a46ced70052ba02d8b59e026926
bipoza <bipoza@gmail.com>
2021-01-27 17:31:43 +0100

    social sharing with font awesome component

--- src/SocialSharing.jsx
+++ src/SocialSharing.jsx
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import PropTypes from 'prop-types';
-// import './volto-cookie-banner.css';
+import { DEFAULT_SOCIAL, DEFAULT_POSITIONS, DEFAULT_LOGO_SIZE, DEFAULT_BUTTON_SIZE } from './defaultSettings';
+import './SocialSharing.css';
+import './fontawesome';
 
-const SocialSharing = () => (
-    <div>
-    <h1>Hemen joango da</h1>
+const SocialSharing = ({
+    socialElements = DEFAULT_SOCIAL,
+    bannerPosition = DEFAULT_POSITIONS,
+    logo_size = DEFAULT_LOGO_SIZE,
+    button_size = DEFAULT_BUTTON_SIZE }) => {
+
+    const [currentUrl, setCurrentUrl] = useState("");
+    useEffect(() => {
+        setCurrentUrl(window.location.href);
+    })
+
+    // MOBILE checker
+    const [width, setWidth] = useState(0);
+    const [isMobile, setIsMobile] = useState(false);
+
+    function handleWindowSizeChange() {
+        // setWidth(window.innerWidth);
+        setIsMobile(window.innerWidth <= 768)
+    }
+    useEffect(() => {
+        handleWindowSizeChange()
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
 
 
+    // end - MOBILE checker
+    return (
+        <div style={isMobile ? bannerPosition["mobile"] : bannerPosition["desktop"]}>
+            <ul className={isMobile ? "horizontal" : "vertical"}>
+                {
+                    socialElements.map(social => {
+                        if ((!social.only_mobile && !isMobile) || (isMobile)) {
 
-    </div>
-//   <CookieConsent
-//     buttonText={buttonText ?? 'OK'}
-//     acceptOnScroll={acceptOnScroll}
-//   >
-//     <div dangerouslySetInnerHTML={{ __html: flattenHTMLToAppURL(text) }} />
-//   </CookieConsent>
-);
+                            return (
+                                <li key={social.id} style={{
+                                    backgroundColor: social.color
+                                }}>
+                                    <div
+                                    style={{width: button_size,
+                                        height: button_size,
+                                        left:`calc(50% - ${button_size} / 2)`
+
+                                    }}
+                                        className="container">
+                                        <a target="_blank" title={`Send to ${social.name}`} href={`${social.sharing_url}${currentUrl}`}  className="icon">
+                                            <FontAwesomeIcon icon={social.fa_name} color="white" size={logo_size} id="icon"/>
+                                        </a>
+                                    </div>
+                                </li>
+                            )
+                        }
+                    })
+                }
+            </ul>
+
+        </div>
+    );
+};
 
 // CookieBanner.propTypes = {
 //   text: PropTypes.string.isRequired,
---
+++ src/defaultSettings.js
@@ -0,0 +1,53 @@
+// import { ReactComponent as Facebook } from '../assets/facebook.svg';
+// import { ReactComponent as Twitter } from '../assets/twitter.svg';
+// import { ReactComponent as Telegram } from '../assets/telegram.svg';
+// import { ReactComponent as Whatsapp } from '../assets/whatsapp.svg';
+
+
+export const DEFAULT_SOCIAL = [{
+    name: "Facebook",
+    fa_name: ["fab", "facebook-f"],
+    color: "#3b5998",
+    sharing_url: "http://www.facebook.com/sharer.php?u=",
+    id: "fb",
+}, {
+    name: "Twitter",
+    fa_name:["fab", "twitter"],
+    color: "#00acee",
+    sharing_url: "http://twitter.com/?status=",
+    id: "tw"
+}, {
+    name: "Telegram",
+    fa_name:["fab", "telegram-plane"],
+    color: "#0088cc",
+    sharing_url: "tg://msg?text=",
+    only_mobile:true,
+    id: "tg"
+}, {
+    name: "Whatsapp",
+    fa_name:["fab", "whatsapp"],
+    color: "#00bb2d",
+    sharing_url: "whatsapp://send?text=",
+    only_mobile:true,
+    id: "wa"
+}];
+
+
+export const DEFAULT_POSITIONS = {
+    "desktop": {
+      position: 'fixed', right: '0', top: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: '9999'
+    },
+    "mobile": {
+      position: 'fixed', left: '50%', bottom: '-23px',
+      transform: 'translate(-50%, -50%)',
+      width: "100%",
+      zIndex: '9999'
+    }
+}
+
+// TYPES:  "lg"|"xs"|"6x"
+export const DEFAULT_LOGO_SIZE = "lg";
+
+export const DEFAULT_BUTTON_SIZE = "30px"
